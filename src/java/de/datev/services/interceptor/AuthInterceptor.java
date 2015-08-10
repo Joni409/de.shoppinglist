274b12f36b3877766e2b4e0fcf6b21db46890445
FabianKress <fabi_kress@online.de>
2015-08-10 12:59:33 +0200

    Authentifikationsklasse hinzugefügt; Anmelden/Registrieren Menü hinzugefügt

---
+++ src/java/de/datev/services/interceptor/AuthInterceptor.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.datev.services.interceptor;
+
+import java.io.IOException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.Provider;
+
+/**
+ *
+ * @author TG00023
+ */
+@Provider
+@PreMatching
+public class AuthInterceptor implements ContainerRequestFilter
+{
+	@Override
+	public void filter (ContainerRequestContext context) throws IOException
+	{
+		String headerValue = context.getHeaderString("X-AUTH");
+		
+		if ((headerValue == null) || (!headerValue.equals("1234")))
+		{
+			//keinen Auth-header gefunden oder falscher Wert in Auth-header
+			ResponseBuilder responseBuilder = Response.status(Status.EXPECTATION_FAILED);
+			Response response = responseBuilder.build();
+			context.abortWith(response);
+		}
+	}
+}
