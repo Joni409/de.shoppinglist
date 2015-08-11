19964844973681c1a0c85d289d23f5f13973bc7d
TG00032 <TG00032@192.168.1.70>
2015-08-11 15:08:15 +0200

    Merged

--- web/js/myJS.js
+++ web/js/myJS.js
@@ -77,8 +77,11 @@ function LoadAllServerLists()
 function LoadSpecificServerList(id)
 {
     $.getJSON("http://localhost:8080/de.datev.shoppinglist/api/lists/" + id, function(result){
-        $.each(result.items, function(index, element){
-            AddElementsToListTable(element.id, element.name, id);
+        $(document).ready(function(){
+            $("#ListeHeadline").text(result.name); 
+            $.each(result.items, function(index, element){
+                AddElementsToListTable(element.id, element.name, id);
+            });
         });
     });
 }
@@ -125,101 +128,21 @@ function UpdateListDataOnServer(id, parentID)
 
 function LoadListTable(id)
 {
+    window.currentid = id;
     $("#MainContainer").empty();
-    $("#MainContainer").load("pages\\itemList.html");
-    LoadSpecificServerList(id);
+    $("#MainContainer").load("pages/itemList.html", function() {
+        LoadSpecificServerList(id);
+    });   
 }
+
 
 function LoadIndex()
 {
     $("#MainContainer").empty();
-    $("#MainContainer").load("index.html #MainContainer > *");
-    LoadAllServerLists();
+    $("#MainContainer").load("index.html #MainContainer > *", function(){
+        LoadAllServerLists();
+    });
 }
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-function LoadConfiguration()
-{
-    $.ajaxSetup({
-        headers:{
-            "X-Auth" : "1234"
-        }
-    });
-};
-
-$("#ColorRandom").change(function()
-{
-    var checked = $("#ColorRandom").prop('checked');
-    $("#ColorSelection").prop('disabled', checked);
-});
-
-$("#AddItemButton").click(function()
-{
-    //Controls auslesen
-    var name = $("#NewItemName").val();
-    var description = $("#NewItemDescription").val();
-    var checked = $("#ColorRandom").prop('checked');
-
-    //Controls zurücksetzen
-    $("#NewItemName").val("");
-    $("#NewItemDescription").val("");
-    $("#ColorRandom").prop('checked', false);
-    $("#ColorSelection").prop('disabled', false);
-
-
-
-    var choosedColor;
-
-    if (checked)
-    {
-        choosedColor = GenerateRandomColor();
-    }
-    else
-    {
-        choosedColor = $("#ColorSelection").val();
-    }
-    
-    //Neues Element an den Server senden und dann neu laden
-    
-    CreateNewServerList(name, description, choosedColor);
-});
-
-function CreateNewServerList(name, beschreibung, color)
-{
-    $.ajax({
-    url: "http://localhost:8080/de.datev.shoppinglist/api/lists/",
-    type: "POST",
-    data: "{\"name\":\"" + name + "\",\"beschreibung\":\"" + beschreibung + "\",\"color\":\"" + color + "\"}",
-    contentType: "application/json"
-});
-}
-
-function LoadAllServerLists()
-{
-    $("#ListContainer").empty();
-    
-    $.getJSON("http://localhost:8080/de.datev.shoppinglist/api/lists/", function(result){
-        $.each(result, function(index, element){
-            AddItemToListContainer(element.id, element.name, element.beschreibung, element.color);
-        });
-    });
-};
-
-function LoadSpecificServerList(id)
-{
-    $.getJSON("http://localhost:8080/de.datev.shoppinglist/api/lists/" + id, function(result){
-        $(document).ready(function() {
-            $("#ListeHeadline").text(result.name); 
-            $.each(result.items, function(index, element){
-                AddElementsToListTable(element.id, element.name);
-            });
-        });
-    });
-};
 
 function ctc(id)
 {
@@ -235,55 +158,7 @@ function ctc(id)
             alert(text);
 
     });
-};
-function GenerateRandomColor()
-{
-    var colors = ['btn-warning', 'btn-info', 'btn-danger', 'btn-success', 'btn-default', 'btn-primary'];
-    var randomNumber = Math.floor(Math.random() * 6);
-    return colors[randomNumber];
-};
-
-function AddItemToListContainer(id, name, description, color)
-{
-    var newListItem = '<div class="col-xs-12 col-md-6 col-lg-3">' +
-            '<a class="btn ' + color + ' listButton" onclick="LoadListTable(' + id + ')">' +
-            '<h2>' + name + '</h2>' +
-            '<p>' + description + '</p>' +
-            '</a>' +
-            '</div>';
-
-    $("#ListContainer").append(newListItem);
-};
-
-function AddElementsToListTable(id, name)
-{
-    var newTableItem =  '<tr>'+
-                            '<td>' + id + '</td>'+
-                            '<td>' + name + '</td>'+
-                            '<td>0,00</td>'+
-                            '<td></td>'+
-							'<td><a class="btn btn-default glyphiconButton" href="#" data-toggle="modal" data-target="#EditItemModal"><span class="glyphicon glyphicon-cog"></span></a></td>' +
-                        '</tr>';
-                
-    $("#ElementsOfListTable").append(newTableItem);      
-};
-
-function LoadListTable(id)
-{
-    window.currentid = id;
-    $("#MainContainer").empty();
-    $("#MainContainer").load("pages/itemList.html", function() {
-        LoadSpecificServerList(id);
-    });   
 }
-
-function LoadIndex()
-{
-    $("#MainContainer").empty();
-    $("#MainContainer").load("index.html #MainContainer > *", function(){
-        LoadAllServerLists();
-    });
-};
 
 function CheckItemDate()
 {
@@ -329,4 +204,4 @@ function CheckItemDate()
             });
         });
     });
-};
+}
