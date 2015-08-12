66fd7043cf7a4d1ef9d4a1d636829ea34456dcf0
TG00024 <TG00024@192.168.1.63>
2015-08-12 13:35:39 +0200

    Löschen Oberfläche

--- web/js/pageFunctions.js
+++ web/js/pageFunctions.js
@@ -104,7 +104,10 @@ function LoadListTable(id)
 function ListDelete(id)
 {
     //Löschen
-    alert("Löschen der Liste mit der ID: " + id);
+    $.ajax({
+        url: 'http://localhost:8080/de.datev.shoppinglist/api/lists/' + id,
+        type: 'DELETE',
+    });
 }
 
 
